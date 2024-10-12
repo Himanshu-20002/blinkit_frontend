b3532fa60eb2b0a438ce4a3cd3a38ec10333208c
Himanshu-20002 <134383327+Himanshu-20002@users.noreply.github.com>
2024-10-12 23:28:51 +0530

    manage to set up all dependencies rigt implemented a new architecture for performence boost

--- src/features/auth/SplashScreen.tsx
+++ src/features/auth/SplashScreen.tsx
@@ -1,11 +1,32 @@
-import { View, StyleSheet } from 'react-native';
-import React from 'react'
+import { View, StyleSheet, Alert } from 'react-native';
+import React, { useEffect } from 'react'
 import { Image } from 'react-native'
 import { Colors } from '../../utils/Constants'
 import logo from '@assets/images/splash_logo.jpeg';
 import { screenHeight, screenWidth } from '../../utils/Scaling';
+import Geolocation from '@react-native-community/geolocation';
+
+Geolocation.setRNConfiguration({
+  skipPermissionRequests:false,
+  authorizationLevel:'always',
+  enableBackgroundLocationUpdates:true,
+  locationProvider:'auto'
+})
+
+
 
 const SplashScreen = () => {
+  useEffect(()=>{
+    const fetchUserLocation = async () => {
+      try {
+      Geolocation.requestAuthorization();
+      } catch (error) {
+          Alert.alert('Error','Sorry, we could not get your location')
+      }
+    }
+    const timeoutId = setTimeout(fetchUserLocation,1000);
+    return () => clearTimeout(timeoutId);
+  },[])
   return (
     <View style={styles.container} >
       <Image source={logo} style={styles.logo}/>
