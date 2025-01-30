1e89f59112f877e6666575f943fe64864373ef5e
Himanshu-20002 <134383327+Himanshu-20002@users.noreply.github.com>
2025-01-30 17:40:04 +0530

    initial working project setup v-0.01

--- ios/nightBucketTests/blinkitTests.m
+++ ios/nightBucketTests/blinkitTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface blinkitTests : XCTestCase
+@interface nightBucketTests : XCTestCase
 
 @end
 
-@implementation blinkitTests
+@implementation nightBucketTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
